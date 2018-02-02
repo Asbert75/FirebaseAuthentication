468745b56a77e6c155adeb3c0ff7f035d3c1997c
Asbert75 <erik_olsson_97@hotmail.com>
2018-02-02 14:26:14 +0100

    .

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const db = firebase.database();
+
+    const loginGithub = document.getElementById("loginGithub");
+    const loginGoogle = document.getElementById("loginGoogle");
+    const loginFacebook = document.getElementById("loginFacebook");
+
+    const logout = document.getElementById("logout");
+    logout.disabled = true;
+
+
+
+}
